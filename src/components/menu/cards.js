57e546d48af790e9ec0b9f7abaeb497a76f7c4cd
Brittney <brittney.postma@hotmail.com>
2020-07-15 20:36:16 -0400

    margin top on cards to give space from carousel

--- src/components/menu/cards.js
+++ src/components/menu/cards.js
@@ -4,7 +4,7 @@ import items from "../../data/menu-items.json"
 
 const Cards = () => {
   return (
-    <div className="grid gap-10 gtc justify-center">
+    <div className="mt-8 grid gap-10 gtc justify-center">
       {items.map(item => (
         <Card name={item.name} price={item.price} image={item.image} />
       ))}
