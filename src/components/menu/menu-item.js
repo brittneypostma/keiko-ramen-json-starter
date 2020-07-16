a101619222a3247165f3f68048a8c20755b740a9
Brittney <brittney.postma@hotmail.com>
2020-07-15 22:17:52 -0400

    styles on menu-item

--- src/components/menu/menu-item.js
+++ src/components/menu/menu-item.js
@@ -4,7 +4,7 @@ import cart from "../../images/cart.png"
 
 const itemWrapper = `rounded-sm bg-white shadow-inset sm:hover:shadow-none flex justify-between items-center pl-5 gap-5 w-full transition-transform ease-linear duration-75 transform sm:hover:scale-105 sm:hover:bg-orange sm:hover:text-white`
 
-const textWrapper = `w-full px-2 py-5 grid justify-center items-center md:flex`
+const textWrapper = `text-black sm:hover:text-white w-full px-2 py-5 grid justify-between items-center md:flex`
 
 const cartLink = `rounded-sm bg-orange self-stretch flex flex-col items-center justify-center p-5`
 
@@ -14,8 +14,8 @@ const MenuItem = ({ name, desc, price }) => {
   return (
     <div className={itemWrapper}>
       <div className={textWrapper}>
-        <h4 className="text-2xl flex-1">{name}</h4>
-        <p className="md:mx-5">{desc}</p>
+        <h4 className="text-2xl whitespace-no-wrap md:whitespace-pre-wrap md:flex-grow w-3/12">{name}</h4>
+        <p className="font-body md:mx-5">{desc}</p>
         <h4 className="text-2xl">${price}</h4>
       </div>
 
