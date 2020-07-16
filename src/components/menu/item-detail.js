d48ef731fbb981b29eb5f0f2ef4b4668484f23c3
Brittney <brittney.postma@hotmail.com>
2020-07-15 21:18:54 -0400

    took out number of items and add to cart on itemdetail and fixed height on image

--- src/components/menu/item-detail.js
+++ src/components/menu/item-detail.js
@@ -10,14 +10,14 @@ const ItemDetail = ({ name, longDesc, price, fullImg }) => {
         <h2 className="text-2xl sm:text-5xl whitespace-no-wrap">{name}</h2>
         <p className="text-xs sm:text-sm xl:text-lg">{longDesc}</p>
         <h2 className="text-red">{price}</h2>
-        <div className="grid md:flex justify-start md:items-center">
+        {/* <div className="grid md:flex justify-start md:items-center">
           <NumberOfItems />
           <div className="mt-2 md:mt-0 md:ml-5">
             <AddToCart />
           </div>
-        </div>
+        </div> */}
       </div>
-      <div className="hidden lg:flex ml-5 xxl:max-h-525">
+      <div className="hidden lg:flex ml-5 xxl:h-450">
         <Img fluid={fullImg} alt={name} className="object-cover w-full" />
       </div>
     </div>
