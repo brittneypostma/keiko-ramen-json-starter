e3387f68f804c32dbdc41a400562c8551cb8b8e2
Brittney <brittney.postma@hotmail.com>
2020-07-16 10:56:14 -0400

    border styles on add to cart button

--- src/components/cart/add-to-cart.js
+++ src/components/cart/add-to-cart.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AddToCart = () => {
   return (
-    <button className="border-2 hover:border-orange hover:border-solid hover:btn-hover ">
+    <button className="border-2 mt-5 md:mt-0 md:ml-5 border-orange hover:border-solid hover:btn-hover">
       ADD TO CART
     </button>
   )
